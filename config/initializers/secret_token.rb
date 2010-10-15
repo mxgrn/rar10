480df3be4d44e4f887becb466d83dd17d24cb45f
Sergei Kozlov <sergei@playcode.nl>
2010-10-15 14:16:42 +0200

    Initial commit: Netzke/ExtJS set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyandrails10::Application.config.secret_token = 'dd5e62916609cd08e38572ece7c7c56f1d70987afded54f2af2907406a67429fbeba338894ebf723511693fd226c895467f89444b78934d1fa9b43e4071f77fa'
